e8aae2b3afd53092fe80ec7f0376f68fcb272741
Ruthenic <mdrakea3@tutanota.com>
2020-12-17 20:36:37 -0500

    Add setup.py for installing program

---
+++ setup.py
@@ -0,0 +1,19 @@
+import subprocess
+print("Welcome to rodder!")
+isUserInstall = input(">< Would you like an install for this user only? [Y/n] ")
+print(">< Cloning rodder git repo...")
+print(subprocess.check_output('git clone https://github.com/ruthenic/rodder ~/.tmp/rodder', shell=True))
+print(">< Moving to installation directory...")
+if isUserInstall.lower() == "y" or isUserInstall.lower() == "":
+    path = '~/.local/rodder'
+    subprocess.call('mv ~/.tmp/rodder ~/.local', shell=True)
+elif isUserInstall.lower() == "n":
+    path = '/usr/local/rodder'
+    print("Input your password to install to root.")
+    subprocess.call('sudo mv ~/.tmp/rodder /usr/local', shell=True)
+else:
+    print("Error: invalid selection! Exiting...")
+    exit()
+subprocess.call('rm -rf ~/.tmp/rodder', shell=True)
+print("Done installing rodder!")
+print("Be sure to add " + path + " to your path!")
