e1d0ec4d421aa0942759520dd79801586f3b5a62
Ruthenic <mdrakea3@tutanota.com>
2020-12-19 17:46:37 -0500

    Begin adding the option to install the latest release, as opposed to the master branch

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import subprocess,os,shutil
+import subprocess,os,shutil,requests,json,zipfile
 print("Welcome to rodder!")
 if os.path.exists(os.getenv('HOME') + '/.config/rodder') == True or os.path.exists(os.getenv('HOME') + '/.local/rodder') == True:
     isDeleteOldInstall = input(">< Previous install found! Would you like to remove it? [y/N] ")
@@ -10,12 +10,29 @@ if os.path.exists(os.getenv('HOME') + '/.config/rodder') == True or os.path.exis
         print(">< Cannot continue. Exiting :(...")
         exit()
 isUserInstall = input(">< Would you like an install for this user only? [Y/n] ")
-print(">< Cloning rodder git repo...")
-print(subprocess.check_output('git clone https://github.com/ruthenic/rodder ' + os.getenv('HOME') + '/.tmp/rodder', shell=True))
+wantsReleaseVersion = input(">< Would you like the latest release, or the master branch? [RELEASE/master] ")
+#print(">< Cloning rodder git repo/downloading latest release...")
+if wantsReleaseVersion.lower() == "master":
+    print(">< Cloning rodder git repo...")
+    print(subprocess.check_output('git clone https://github.com/ruthenic/rodder ' + os.getenv('HOME') + '/.tmp/rodder', shell=True))
+elif wantsReleaseVersion.lower() == "release":
+    print(">< Downloading latest rodder release...")
+    dlurl = 'https://api.github.com/repos/Ruthenic/rodder/zipball/v0.1'
+    file = requests.get(dlurl)
+    os.makedirs(os.getenv('HOME') + '/.tmp/rodder', exist_ok=True)
+    print(">< Extracting latest rodder release...")
+    with open(os.getenv('HOME') + '/.tmp/rodder/rodder-v0.1.zip', 'wb') as f:
+        f.write(file.content)
+    with zipfile.ZipFile(os.getenv('HOME') + '/.tmp/rodder/rodder-v0.1.zip') as f:
+        f.extractall(os.getenv('HOME') + '/.tmp/rodder')
 print(">< Moving to installation directory...")
 if isUserInstall.lower() == "y" or isUserInstall.lower() == "":
     path = '~/.local/rodder'
-    shutil.move(os.getenv('HOME') + '/.tmp/rodder', os.getenv('HOME') + '/.local')
+    if wantsReleaseVersion.lower() == "master":
+        shutil.move(os.getenv('HOME') + '/.tmp/rodder', os.getenv('HOME') + '/.local')
+    else:
+        shutil.move(os.getenv('HOME') + '/.tmp/rodder/Ruthenic-rodder-8bb63e2', os.getenv('HOME') + '/.local')
+        os.rename(os.getenv('HOME') + '/.local/Ruthenic-rodder-8bb63e2', os.getenv('HOME') + '/.local/rodder')
 elif isUserInstall.lower() == "n":
     path = '/usr/local/rodder'
     print("Input your password to install to root (WARNING: NOT FULLY FUNCTIONAL, NOR FULLY TESTED.).")
