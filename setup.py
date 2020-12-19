c8ef8dd1a13204bb5479a0862da4821c8df9b38e
Ruthenic <mdrakea3@tutanota.com>
2020-12-18 21:07:23 -0500

    Convert rest of setup.py to native python calls, fix for rodder update

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ if os.path.exists(os.getenv('HOME') + '/.config/rodder') == True or os.path.exis
     if isDeleteOldInstall.lower() == 'y':
         shutil.rmtree(os.getenv('HOME') + '/.local/rodder')
         shutil.rmtree(os.getenv('HOME') + '/.config/rodder')
-        shutil.rmtree(os.getenv('HOME') + '/.tmp/rodder')
+        #shutil.rmtree(os.getenv('HOME') + '/.tmp/rodder')
     else:
         print(">< Cannot continue. Exiting :(...")
         exit()
 isUserInstall = input(">< Would you like an install for this user only? [Y/n] ")
 print(">< Cloning rodder git repo...")
-print(subprocess.check_output('git clone https://github.com/ruthenic/rodder ~/.tmp/rodder', shell=True))
+print(subprocess.check_output('git clone https://github.com/ruthenic/rodder ' + os.getenv('HOME') + '/.tmp/rodder', shell=True))
 print(">< Moving to installation directory...")
 if isUserInstall.lower() == "y" or isUserInstall.lower() == "":
     path = '~/.local/rodder'
-    subprocess.call('mv ~/.tmp/rodder ~/.local', shell=True)
+    shutil.move(os.getenv('HOME') + '/.tmp/rodder', os.getenv('HOME') + '/.local')
 elif isUserInstall.lower() == "n":
     path = '/usr/local/rodder'
     print("Input your password to install to root (WARNING: NOT FULLY FUNCTIONAL, NOR FULLY TESTED.).")
@@ -24,9 +24,11 @@ else:
     print("Error: invalid selection! Exiting...")
     exit()
 print(">< Creating config file...")
-subprocess.call('mkdir ~/.config/rodder && mkdir ~/.config/rodder/repos', shell=True)
-subprocess.call('echo "https://github.com/ruthenic/rodder-repo" >> ~/.config/rodder/repos/master-repo-list.txt', shell=True)
-subprocess.call('rm -rf ~/.tmp/rodder', shell=True)
+os.makedirs(os.getenv('HOME') + '/.config/rodder')
+os.makedirs(os.getenv('HOME') + '/.config/rodder/repos')
+with open(os.getenv('HOME') + '/.config/rodder/repos/master-repo-list.txt', 'w') as f:
+    f.write('https://github.com/ruthenic/rodder-repo')
+#shutil.rmtree(os.getenv('HOME') + '/.tmp/rodder')
 subprocess.call('export PATH=' + os.getenv('HOME') + '/.local/rodder:$PATH', shell=True)
 print("Done installing rodder!")
 print("Be sure to add " + path + " to your path with " + '`export PATH=' + os.getenv('HOME') + '/.local/rodder:$PATH`')
