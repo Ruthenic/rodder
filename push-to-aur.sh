25ebf4d996e012bf9604eae8e227665cf83a3c37
Ruthenic <mdrakea3@tutanota.com>
2020-12-22 19:29:51 -0500

    Add script to automatically push to aur, remove PKGBUILD from git repo, update .gitignore

---
+++ push-to-aur.sh
@@ -0,0 +1,6 @@
+#note: this only works on my machine, but i should be the only one running it anyways
+cd ~/Coding/Etc/rodder-aur/rodder-git
+makepkg --printsrcinfo > .SRCINFO
+git add PKGBUILD .SRCINFO
+git commit
+git push
