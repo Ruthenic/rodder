0e9199149b75fca9602ae0403f5067228cba243d
Ruthenic <mdrakea3@tutanota.com>
2020-12-22 19:39:25 -0500

    Update AUR script

--- push-to-aur.sh
+++ push-to-aur.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 #note: this only works on my machine, but i should be the only one running it anyways
 cd ~/Coding/Etc/rodder-aur/rodder-git
+makepkg
 makepkg --printsrcinfo > .SRCINFO
 git add PKGBUILD .SRCINFO
 git commit
