94ec497702f7bb24e47e8ebe9518199dd919bb8d
Drake <mdrakea3@tutanota.com>
2021-01-31 16:21:47 -0500

    edit push-to-aur.sh

--- push-to-aur.sh
+++ push-to-aur.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #note: this only works on my machine, but i should be the only one running it anyways
-cd ~/Coding/Etc/rodder-aur/rodder-git
+cd ~/Coding/Etc/rodder-aur
 makepkg
 makepkg --printsrcinfo > .SRCINFO
 git add PKGBUILD .SRCINFO
